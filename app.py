a51743192717d004f3a7db387ed79b695008fd61
André Mendes Costa <andre.mendes.sp01@gmail.com>
2024-07-12 09:44:19 -0300

    aula 53. implementando o login e usuario, mas com problema para importar a JWTManager da biblioteca flask_jwt_extended

--- app.py
+++ app.py
@@ -1,29 +1,37 @@
 from flask import Flask
 from flask_restful import Api
-from resources.hotel import Hoteis, Hotel
-from resources.user import User, UserRegister
+from flask_jwt_extended import JWTManager
 from sql_alchemy import banco
+from resources.hotel import Hoteis, Hotel
+from resources.user import User, UserRegister, UserLogin
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///banco.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 api = Api(app)
+jwt = JWTManager(app)
 
 # Solução para criar o banco de dados antes da primeira requisição
 @app.before_first_request
 def cria_banco():
-        banco.create_all()
+    banco.create_all()
 
-# Adicionando o recurso à rota /hoteis
+# Adicionando o recurso à rota
 api.add_resource(Hoteis, '/hoteis')
 api.add_resource(Hotel, '/hoteis/<string:hotel_id>')
-api.add_resource(User, '/usuarios/<int:user_id>') 
+api.add_resource(User, '/usuarios/<int:user_id>')
 api.add_resource(UserRegister, '/cadastro')
-
+api.add_resource(UserLogin, '/login')
 
 if __name__ == '__main__':
     banco.init_app(app)
     app.run(debug=True)
 
+print("JWTManager imported successfully")
+
 
 # http://127.0.0.1:500/hoteis
+
+# ativar ambiente virtual windows '.\ambvir\Scripts\Activate'
+# subir a aplicação 'python3 app.py'
--- resources/user.py
+++ resources/user.py
@@ -1,5 +1,11 @@
 from flask_restful import Resource, reqparse
 from models.user import UserModel
+from flask_jwt_extended import create_access_token
+from werkzeug.security import safe_str_cmp
+
+argumentos = reqparse.RequestParser()
+argumentos.add_argument('login',  type=str, required=True, help="The filed 'login' cannot be left blank.")
+argumentos.add_argument('senha', type=str, required=True, help="The find 'senha' cannot be left blank." )
 
 class User(Resource):
     #/usuarios/{user_id}
@@ -23,9 +29,6 @@ class User(Resource):
 class UserRegister(Resource):
     #/cadastro
     def post(self):
-        argumentos = reqparse.RequestParser()
-        argumentos.add_argument('login',  type=str, required=True, help="The filed 'login' cannot be left blank.")
-        argumentos.add_argument('senha', type=str, required=True, help="The find 'senha' cannot be left blank." )
         dados = argumentos.parse_args()
 
         if UserModel.find_by_login(dados ['login']):
@@ -34,3 +37,17 @@ class UserRegister(Resource):
         user = UserModel(**dados) #(dados['login'], dados['senha']) #instanciando um novo objeto 
         user.save_user()
         return{'message':'User created sucessfully!'},201 #created
+    
+class UserLogin(Resource):
+   
+    @classmethod
+    def post(cls):
+        dados = argumentos.parse_args()
+
+        user = UserModel.find_by_login(dados['login'])
+
+        if user and safe_str_cmp(user.senha, dados['senha']):
+            token_de_acesso = create_access_token(identity=user.user_id)
+            return  {'access_token': token_de_acesso},200
+        return {'message': 'The username or password is incorrect.'},401 #Unathorized 
+    
