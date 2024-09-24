430415ae0d794161738f3a1d25983d7806ba1de3
André Mendes Costa <andre.mendes.sp01@gmail.com>
2024-09-24 10:50:08 -0300

    corrigindo o self.site para sel.site_id, para na criação de um novo hotel, ele não retornar como 'site_id': null

--- resources/site.py
+++ resources/site.py
@@ -2,30 +2,32 @@ from flask_restful import Resource
 from models.site import SiteModel
 
 class Sites(Resource):
-    def get (self):
-        return {'sites':[site.json() for site in SiteModel.query.all()]}
-    
+    def get(self):
+        return {'sites': [site.json() for site in SiteModel.query.all()]}
+
 class Site(Resource):
-    def get (self, url):
+    def get(self, url):
         site = SiteModel.find_site(url)
         if site:
-            return site.json
-        return {'message':'site not found.'}, 404 # not found
-        pass
+            return site.json()
+        return {'message': 'Site not found.'}, 404  # not found
 
-    def get (self, url):
+    def post(self, url):
         if SiteModel.find_site(url):
-            return {"message":"The site '{}'already exists".format(url)}, 400 # bad request
+            return {"message": "The site '{}' already exists.".format(url)}, 400  # bad request
         site = SiteModel(url)
         try:
             site.save_site()
         except:
-            return {'message': 'An internal error ocurred trying to create a new site.'}, 500
-        return site.json 
-    
-    def delete(self,url):
+            return {'message': 'An internal error occurred trying to create a new site.'}, 500
+        return site.json(), 201  # site criado com sucesso
+
+    def delete(self, url):
         site = SiteModel.find_site(url)
         if site:
-            site.delete_site
-            return {'message': 'Site deleted'}
-        return {'message': 'Site not found'},404
+            try:
+                site.delete_site()
+                return {'message': 'Site deleted'}
+            except:
+                return {'message': 'An internal error occurred while trying to delete the site.'}, 500
+        return {'message': 'Site not found'}, 404
